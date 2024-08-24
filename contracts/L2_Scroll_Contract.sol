2feabd47e36e6d55627028c33bf28385f85ea7ce
Icer Liang <liangshuang@sutu.tech>
2024-08-25 00:40:08 +0900

    generate solidity interface code and connect wallet

--- contracts/L2_Scroll_Contract.sol
+++ contracts/L2_Scroll_Contract.sol
@@ -3,10 +3,22 @@ pragma solidity ^0.8.0;
 
 // Interface for the verifier contract
 interface IVerifier {
-  function verifyProof(bytes memory proof) external view returns (bool);
+  function verifyProof(
+    bytes memory proof,
+    bytes32[] memory publics
+  ) external view returns (bool);
 }
 
 contract L2Contract {
+  struct Task {
+    string name;
+    string rule;
+    uint256 deadline;
+  }
+
+  mapping(uint256 => Task) private tasks;
+  uint256 private taskCounter;
+
   address public verifierContract;
   mapping(bytes32 => bytes32) public heightCommitments;
   mapping(bytes32 => mapping(uint256 => bytes32)) public dailyCommitments;
@@ -16,26 +28,48 @@ contract L2Contract {
     verifierContract = _verifierContract;
   }
 
-  function get_tasks() public view returns (bytes32[] memory) {
-    bytes32[] memory tasks;
-
-    // address L1_SLOAD_ADDRESS = 0x101;
-    //  (bool success, bytes memory ret) =L1_SLOAD_ADDRESS.staticcall(
-    // abi.encodePacked( llAddress, keyl, key2));
-    // if (!success) {
-    // revert( "LISLOAD failed" );
-    // }
-    // return abi.decode( ret, (uint256, uint256));
-
-    return tasks;
+  function publish_task(
+    string memory name,
+    string memory rule,
+    uint256 deadline
+  ) public {
+    tasks[taskCounter] = Task(name, rule, deadline);
+    taskCounter++;
   }
+
+  function get_tasks() public view returns (Task[] memory) {
+    Task[] memory taskList = new Task[](taskCounter);
+    for (uint256 i = 0; i < taskCounter; i++) {
+      taskList[i] = tasks[i];
+    }
+    return taskList;
+  }
+
+  // function get_tasks() public view returns (bytes32[] memory) {
+  //   bytes32[] memory tasks;
+
+  //   // address L1_SLOAD_ADDRESS = 0x101;
+  //   //  (bool success, bytes memory ret) =L1_SLOAD_ADDRESS.staticcall(
+  //   // abi.encodePacked( llAddress, keyl, key2));
+  //   // if (!success) {
+  //   // revert( "LISLOAD failed" );
+  //   // }
+  //   // return abi.decode( ret, (uint256, uint256));
+
+  //   return tasks;
+  // }
 
   function verify_task(
     bytes32 pubkey,
     bytes memory proof
   ) public view returns (bool) {
     IVerifier verifier = IVerifier(verifierContract);
-    return verifier.verifyProof(proof);
+    uint256 dayNumber = block.timestamp / 1 days;
+    bytes32[] memory commitments = new bytes32[](30);
+    for (uint256 i = 0; i < 30; i++) {
+      commitments[i] = dailyCommitments[pubkey][dayNumber];
+    }
+    return verifier.verifyProof(proof, commitments);
   }
 
   function create_account(bytes32 pubkey, bytes32 height_commitment) public {
--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -14,6 +14,35 @@
         <q-toolbar-title> E-thin </q-toolbar-title>
 
         <!-- <div>Quasar v{{ $q.version }}</div> -->
+        <ConnectWalletButton
+          :address="address"
+          :txnCount="0"
+          :dark="false"
+          @click="connect"
+        >
+          <!-- Override the default text on the "Connect Wallet" button -->
+          Connect Wallet!
+
+          <template #pending>
+            <!-- Override the blue pending transaction div -->
+          </template>
+
+          <template #spinner>
+            <!-- Use your own Spinner (svg, css, image, etc.) -->
+          </template>
+
+          <template #identicon>
+            <!-- Use your own identicon library. Jazzicon is the default -->
+          </template>
+
+          <template #connectWalletButton>
+            <!-- Use your own button for the "Connect Wallet" state when no address is provided -->
+          </template>
+
+          <template #addressButton>
+            <!-- Use your own button to display the address when address is provided. Does not remove the pending transaction div -->
+          </template>
+        </ConnectWalletButton>
       </q-toolbar>
     </q-header>
 
@@ -66,6 +95,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { ConnectWalletButton, useMetaMaskWallet } from 'vue-connect-wallet';
 
 defineOptions({
   name: 'MainLayout',
@@ -78,4 +108,34 @@ const tab = ref('mails');
 // function toggleLeftDrawer() {
 //   leftDrawerOpen.value = !leftDrawerOpen.value;
 // }
+
+const wallet = useMetaMaskWallet();
+
+const address = ref('');
+
+wallet.onAccountsChanged((accounts: string[]) => {
+  console.log('account changed to: ', accounts[0]);
+});
+wallet.onChainChanged((chainId: number) => {
+  console.log('chain changed to:', chainId);
+});
+
+const connect = async () => {
+  console.log('connect');
+  const accounts = await wallet.connect();
+  if (typeof accounts === 'string') {
+    console.log('An error occurred' + accounts);
+  }
+  address.value = accounts[0];
+};
+
+const isConnected = async () => {
+  const accounts = await wallet.getAccounts();
+  if (typeof accounts === 'string') return false;
+  return accounts.length > 0;
+};
+
+isConnected().then((value) => {
+  if (value) connect();
+});
 </script>
